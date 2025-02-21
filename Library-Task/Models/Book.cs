9ee742d62fe6c4818fb97c8aea7fe0d04024e82c
Daniel De La Pena <del17011@byui.edu>
2025-02-20 19:12:55 -0700

    project set up and db context

---
+++ Library-Task/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Task.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required int PublishedYear { get; set; }
+    }
+}
---
+++ Library-Task/Models/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Task.Models
+{
+    public class BookViewModel
+    {
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required string PublishedYearDisplay { get; set; }
+
+    }
+}
